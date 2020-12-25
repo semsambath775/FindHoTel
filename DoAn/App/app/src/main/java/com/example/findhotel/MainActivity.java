0f26cf03528878c9b576398109d624e661df3dd8
semsambath775 <76578329+semsambath775@users.noreply.github.com>
2020-12-25 12:30:20 +0700

    upload

---
+++ DoAn/App/app/src/main/java/com/example/findhotel/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.findhotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
---
+++ DoAn/App/app/src/main/java/com/example/findhotel/model/RecentsData.java
@@ -0,0 +1,11 @@
+package com.example.findhotel.model;
+
+public class RecentsData {
+    String placeName;
+    String countryName;
+    String price;
+    
+
+
+
+}
